adcc53c35ace260f0a004cedd97a5da9a41f4a1b
Alex <mika@Mikabook.local>
2026-07-16 14:41:49 +0100

    improve plugin.cmake

--- api/plugin.cmake
+++ api/plugin.cmake
@@ -2,11 +2,13 @@ cmake_minimum_required(VERSION 3.10)
 
 get_filename_component(NOTOJS_API_DIR ${CMAKE_CURRENT_LIST_DIR} REALPATH)
 get_filename_component(NOTOJS_SRC_DIR ${NOTOJS_API_DIR} DIRECTORY)
+add_compile_options(-Wno-c99-designator)
 
 macro(plugin name)
 
 add_library(${name} SHARED ${name}.cpp)
 target_link_libraries(${name} PRIVATE Boost::boost)
+target_include_directories(${name} PRIVATE ${NOTOJS_SRC_DIR}/api)
 target_include_directories(${name} PRIVATE ${NOTOJS_SRC_DIR}/lib)
 target_include_directories(${name} PRIVATE ${NOTOJS_SRC_DIR}/lib/rapidjson/include)
 
@@ -20,4 +22,8 @@ if(APPLE)
         LINK_FLAGS "-undefined dynamic_lookup"
     )
 endif()
+endmacro()
+
+macro(module name)
+plugin(${name})
 endmacro()
